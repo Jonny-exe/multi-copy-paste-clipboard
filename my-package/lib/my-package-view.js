871a1525721c96fb0b862fa46f060a566b5bca5e
Jonny-exe <69385502+Jonny-exe@users.noreply.github.com>
2020-09-08 13:22:49 +0200

    update

---
+++ my-package/lib/my-package-view.js
@@ -0,0 +1,84 @@
+'use babel';
+
+export default class MyPackageView {
+
+
+  constructor(serializedState) {
+    // Create root element
+    this.element = document.createElement('div');
+    this.element.classList.add('my-package2');
+    // const bigDiv = document.createElement('div');
+    // this.element.appendChild(bigDiv);
+    // bigDiv.classList.add('bigDiv');
+    var inputMain = document.createElement("INPUT");
+    var inputTwo = document.createElement("INPUT");
+
+    // var form = document.createElement("FORM");
+    inputMain.setAttribute("type", "number");
+    inputMain.setAttribute("id", "input");
+    inputMain.setAttribute("class", "input");
+
+
+
+
+    //Create message element
+
+    const message = document.createElement('div');
+    const inputs = document.createElement('div');
+
+    message.textContent = 'These are your saved "copies", you can paste them typing the number of the "copy" in the text input';
+    message.classList.add('exampletext');
+    this.element.appendChild(message);
+    // this.element.appendChild(form);
+    this.element.appendChild(inputs);
+    inputs.appendChild(inputMain);
+
+
+    // this.getLocation();
+  }
+
+
+  // Returns an object that can be retrieved when package is activated
+  serialize() {}
+
+  // Tear down any state and detach
+  destroy() {
+    this.element.remove();
+  }
+
+  getElement() {
+    return this.element;
+  }
+
+  getSelectedText() {
+    return this.getLastSelection().getText();
+  }
+  getText() {
+    return this.editor.buffer.getTextInRange(this.getBufferRange());
+  }
+
+  resetInput() {
+    document.getElementById('input').focus();
+    document.getElementById('input').value = '';
+  }
+
+  addSuccess(message, options) {
+    return this.addNotification(new Notification('success', 'message', options));
+  }
+
+  setCopy(copy, copyNum, copyArray) {
+    for (let i = 0; i < document.getElementsByClassName('myDiv').length; i + 1) {
+      console.log('Remove numer ' + i);
+      document.getElementsByClassName('myDiv')[i].remove();
+    }
+
+    for (let a = 0; a < copyArray.length; a++) {
+      console.log('Add numer ' + a);
+      const message = document.createElement('div');
+      message.textContent = (a + 1) + " - " + copyArray[a];
+      message.classList.add('myDiv');
+      this.element.appendChild(message);
+    }
+    console.log(document.getElementsByClassName('myDiv').length);
+  }
+}
--- my-package/lib/my-package.js
+++ my-package/lib/my-package.js
@@ -28,8 +28,18 @@ export default {
     // Register command that toggles this view
     this.subscriptions.add(atom.commands.add('atom-workspace', {
       'my-package:copy': () => this.copy(),
-      'my-package:paste': () => this.paste(),
-      'my-package:toggle': () => this.see()
+      'my-package:paste': () => this.paste(1),
+      'my-package:toggle': () => this.see(),
+      'my-package:paste-1': () => this.paste(1),
+      'my-package:paste-2': () => this.paste(2),
+      'my-package:paste-3': () => this.paste(3),
+      'my-package:paste-4': () => this.paste(4),
+      'my-package:paste-5': () => this.paste(5),
+      'my-package:paste-6': () => this.paste(6),
+      'my-package:paste-7': () => this.paste(7),
+      'my-package:paste-8': () => this.paste(8),
+      'my-package:paste-9': () => this.paste(9),
+      'my-package:paste-10': () => this.paste(10)
     }));
   },
 
@@ -52,22 +62,33 @@ export default {
       copy = selection;
     }
     if (copy !== lastCopy) {
+      copyArray.reverse();
+      copyArray.push(copy);
+      copyArray.reverse();
       lastCopy = copy;
       copyNum += +1;
-      this.myPackageView.setCopy(copy, copyNum);
+      if (10 < copyArray.length) {
+        copyArray = copyArray.slice(0, 10);
+      }
+
+      this.myPackageView.setCopy(copy, copyNum, copyArray);
     }
   },
 
-  paste() {
+  paste(number) {
     let editor;
 
-    console.log('MyPackage was toggled!');
     if (editor = atom.workspace.getActiveTextEditor()) {
-      let selection = editor.getSelectedText()
-      let reversed = selection.split('').reverse().join('')
-      editor.insertText(copy)
+      editor.insertText(copyArray[number - 1]);
     }
 
+  },
+
+
+  startEnter() {
+    var number = parseInt(document.getElementById('input').value);
+    console.log(number);
+    this.paste(number);
   },
 
   see() {
@@ -75,8 +96,10 @@ export default {
 
     if (this.modalPanel.isVisible()) {
       this.modalPanel.hide();
+      this.startEnter();
     } else {
       this.modalPanel.show();
+      this.myPackageView.resetInput();
     }
   },
 
